a1c5f7afb2c957eb31f5760f0da173a7f3fcd982
Rulba <ra.torresy@duocuc.cl>
2025-05-20 15:01:18 -0400

    sse noti

--- backend/static/script.js
+++ backend/static/script.js
@@ -210,3 +210,10 @@ document.getElementById('vender').addEventListener('click', async () => {
 });
 
 document.addEventListener('DOMContentLoaded', cargarDatos);
+
+const evtSource = new EventSource('/sse');
+evtSource.onmessage = function (event) {
+  const alerta = document.getElementById('alerta-stock');
+  alerta.style.display = 'block';
+  alerta.innerText = event.data;
+};
